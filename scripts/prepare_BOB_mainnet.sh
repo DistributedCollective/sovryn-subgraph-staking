16f117bdbc6923d21661ec124968323aff2ac15d
Rytis Grincevičius <rytis.grincevicius@gmail.com>
2024-04-23 11:09:10 +0300

    feat: mainnet staking subgraph

---
+++ scripts/prepare_BOB_mainnet.sh
@@ -0,0 +1,13 @@
+export NETWORK=mainnet;
+red=`tput setaf 1`
+yellow=`tput setaf 3`
+green=`tput setaf 2`
+reset=`tput sgr0`
+echo "Preparing subgraph for ${yellow}$NETWORK${reset} network" && echo;
+npx mustache config/BOB.mainnet.json subgraph.template.yaml > subgraph.yaml;
+echo "File: ${green}subgraph.yaml${reset} has been mustached";
+npx mustache config/BOB.mainnet.json docker-compose.template.yml > docker-compose.yml;
+echo "File: ${green}docker-compose.yml${reset} has been mustached";
+npx mustache config/BOB.mainnet.json src/constants.template.ts > src/constants.ts;
+
+echo "File: ${green}src/config/contracts.ts${reset} has been mustached";
